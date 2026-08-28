7e3f1fea6676d2b9d4c4c4cda1a8180c66078537
mohamedgamalshaltot <mohmedgamalshaltot@gmail.com>
2026-08-28 16:52:48 +0300

    Question1

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -5,6 +5,20 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            #region Question01
+            //1.Create a Book class with a Title (string) and Pages (int). Create a Book object and store it
+            //in a variable of type object.Print it.
+            // object mybook = new Book { Title = "C# Programming", Pages = 350 };
+            //Console.WriteLine(mybook);
+            #endregion
         }
     }
+    public class Book
+    {
+        public string Title { get; set; }
+        public int Pages { get; set; }
+       
+
+
+
 }
