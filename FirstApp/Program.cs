efec7479243b76eec1b907dea773ab5985179ab7
mohamedgamalshaltot <mohmedgamalshaltot@gmail.com>
2026-08-28 21:13:53 +0300

    Uncomment Q1, Q2, Q3

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -8,23 +8,23 @@
             #region Question01
             //1.Create a Book class with a Title (string) and Pages (int). Create a Book object and store it
             //in a variable of type object.Print it.
-            // object mybook = new Book { Title = "C# Programming", Pages = 350 };
-            //Console.WriteLine(mybook);
+            object mybook = new book { title = "c# programming", pages = 350 };
+            console.writeline(mybook);
             #endregion
             #region Question02
             //2.Using the Book class above, print the result of calling ToString(), Equals() (compare book
             // with itself), GetHashCode(), and GetType() on book.
-            //object mybook = new Book { Title = "C# Programming", Pages = 350 };
-            //Console.WriteLine(mybook.ToString());
-            //Console.WriteLine(mybook.Equals(mybook));
-            //Console.WriteLine(mybook.GetHashCode());
-            //Console.WriteLine(mybook.GetType());
+            object mybook = new Book { Title = "C# Programming", Pages = 350 };
+            Console.WriteLine(mybook.ToString());
+            Console.WriteLine(mybook.Equals(mybook));
+            Console.WriteLine(mybook.GetHashCode());
+            Console.WriteLine(mybook.GetType());
             #endregion
             #region Question03
             //3.Look at the line below. Is it a compile-time error, a runtime error, or a logical error? Fix it.
             //int pages = "464";
-            //Answer: This is a compile-time error because you cannot put text (string) into an int varible.
-            // int pages = 464;
+             Answer: This is a compile - time error because you cannot put text(string) into an int varible.
+             int pages = 464;
             #endregion
             #region Question04
 
