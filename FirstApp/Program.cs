e9afd5e4fd4b9e92fc8ae412a90c9d1fc4e01ee3
mohamedgamalshaltot <mohmedgamalshaltot@gmail.com>
2026-08-28 17:45:54 +0300

    Question 2

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -11,14 +11,20 @@
             // object mybook = new Book { Title = "C# Programming", Pages = 350 };
             //Console.WriteLine(mybook);
             #endregion
+            #region Question02
+            //2.Using the Book class above, print the result of calling ToString(), Equals() (compare book
+            // with itself), GetHashCode(), and GetType() on book.
+            //object mybook = new Book { Title = "C# Programming", Pages = 350 };
+            //Console.WriteLine(mybook.ToString());
+            //Console.WriteLine(mybook.Equals(mybook));
+            //Console.WriteLine(mybook.GetHashCode());
+            //Console.WriteLine(mybook.GetType());
+            #endregion
         }
     }
     public class Book
     {
         public string Title { get; set; }
         public int Pages { get; set; }
-       
-
-
-
+    }
 }
