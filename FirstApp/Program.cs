6c6a84c035e7f8e8d25231fe0a6b57f41975736c
mohamedgamalshaltot <mohmedgamalshaltot@gmail.com>
2026-08-28 17:45:54 +0300

    Question03

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -11,14 +11,28 @@
             // object mybook = new Book { Title = "C# Programming", Pages = 350 };
             //Console.WriteLine(mybook);
             #endregion
+            #region Question02
+            //2.Using the Book class above, print the result of calling ToString(), Equals() (compare book
+            // with itself), GetHashCode(), and GetType() on book.
+            //object mybook = new Book { Title = "C# Programming", Pages = 350 };
+            //Console.WriteLine(mybook.ToString());
+            //Console.WriteLine(mybook.Equals(mybook));
+            //Console.WriteLine(mybook.GetHashCode());
+            //Console.WriteLine(mybook.GetType());
+            #endregion
+            #region Question03
+            //3.Look at the line below. Is it a compile-time error, a runtime error, or a logical error? Fix it.
+            //int pages = "464";
+            //Answer: This is a compile-time error because you cannot put text (string) into an int varible.
+            // int pages = 464;
+            #endregion
+
         }
+
     }
     public class Book
     {
         public string Title { get; set; }
         public int Pages { get; set; }
-       
-
-
-
+    }
 }
